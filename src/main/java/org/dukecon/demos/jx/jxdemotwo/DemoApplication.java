959672f03a6318f4060843239a9f8b3ec1f5ce58
Gerd Aschemann <gerd@aschemann.net>
2018-07-24 23:23:50 +0200

    Initial import

---
+++ src/main/java/org/dukecon/demos/jx/jxdemotwo/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.dukecon.demos.jx.jxdemotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
